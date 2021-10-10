8a0b13ee4f40bf9f5a3126c8b54f5cf7b122aca2
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2021-10-10 22:02:07 +0100

    ch-corrected adapters to use viewBinding

--- app/src/main/java/com/example/emmy_ibook/adapter/ExploreCategoriesAdapter.kt
+++ app/src/main/java/com/example/emmy_ibook/adapter/ExploreCategoriesAdapter.kt
@@ -1,28 +1,25 @@
 package com.example.emmy_ibook.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.emmy_ibook.R
+import com.example.emmy_ibook.databinding.ExploreCategoriesListBinding
 import com.example.emmy_ibook.fragments.HomeFragment
 import com.example.emmy_ibook.model.ExploreBooks
 
-class ExploreCategoriesAdapter(private val context: HomeFragment, private val dataSet: List<ExploreBooks> ) : RecyclerView.Adapter<ExploreCategoriesAdapter.ExploreCategoriesViewHolder>() {
+class ExploreCategoriesAdapter(private val context: HomeFragment, private val dataSet: List<ExploreBooks> )
+    : RecyclerView.Adapter<ExploreCategoriesAdapter.ExploreCategoriesViewHolder>() {
 
-    class ExploreCategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val bkImage : ImageView = view.findViewById(R.id.bk_img)
-        val bkDescription : TextView = view.findViewById(R.id.bk_description)
+    class ExploreCategoriesViewHolder(binding: ExploreCategoriesListBinding) : RecyclerView.ViewHolder(binding.root){
+        val bkImage : ImageView = binding.bkImg
+        val bkDescription : TextView = binding.bkDescription
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ExploreCategoriesViewHolder {
-        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.explore_categories_list, parent, false)
-        return ExploreCategoriesViewHolder(adapterLayout)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExploreCategoriesViewHolder {
+        val binding = ExploreCategoriesListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ExploreCategoriesViewHolder(binding)
     }
 
     override fun onBindViewHolder(
