2288e7e53889592f5703ad0511922b6a74ee0a5b
folaoluwafemi <folaoluwafemi55@gmail.com>
2021-10-12 20:04:46 +0100

    pre-merge commit

--- app/src/main/java/com/example/emmy_ibook/fragments/AddNewBook.kt
+++ app/src/main/java/com/example/emmy_ibook/fragments/AddNewBook.kt
@@ -1,40 +1,40 @@
 package com.example.emmy_ibook.fragments
 
-import android.content.Context
 import android.os.Bundle
 import android.view.ContextThemeWrapper
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.emmy_ibook.R
 import com.example.emmy_ibook.databinding.FragmentAddNewBookBinding
 
 
-class add_new_book : Fragment() {
+class AddNewBook : Fragment() {
     private var _binding: FragmentAddNewBookBinding? = null
     private val binding get()= _binding!!
-    private var navCon: NavController? = null
+    private lateinit var navCon: NavController
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val themeWrapperContext = ContextThemeWrapper(this.activity, R.style.AddNewBookTheme)
-        val localInflater: LayoutInflater = inflater.cloneInContext(themeWrapperContext)
-        _binding = FragmentAddNewBookBinding.inflate(localInflater, container, false, )
+        _binding = FragmentAddNewBookBinding.inflate(inflater, container, false)
         navCon = NavHostFragment.findNavController(this)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.backArrowBtn.setOnClickListener {
+            navCon.navigateUp()
+        }
         binding.addBtn.setOnClickListener {
-            //add code to navigate from this fragment to the category fragment here using "navCon [already initialised]"
+            //add code to navigate from "this fragment" to "category fragment" here using "navCon [already initialised]".
         }
         binding.donateHardCopyBtn.setOnClickListener {
-            //add code to navigate from this fragment to the 
+            //add code to navigate from "this fragment" to the "donate a hard copy book fragment" here.
         }
     }
 
