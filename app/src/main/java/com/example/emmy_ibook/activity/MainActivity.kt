ed7685bf1efe9a526858d1292493660edec06b65
The_SHOGBAMU <shogbamuolamide@gmail.com>
2021-10-16 08:20:48 +0100

    navigation resolved

--- app/src/main/java/com/example/emmy_ibook/activity/MainActivity.kt
+++ app/src/main/java/com/example/emmy_ibook/activity/MainActivity.kt
@@ -8,20 +8,19 @@ import com.example.emmy_ibook.fragments.NotificationFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_home_screen.*
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
     val manager = supportFragmentManager
 
     private val mOnNavigationItemReselectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId){
-            R.id.ic_home -> {
-                createHomeFragment()
-                return@OnNavigationItemSelectedListener true
-            }
+
             R.id.ic_notification -> {
                 createNotificationFragment()
                 return@OnNavigationItemSelectedListener true
             }
+
         }
         false
     }
@@ -35,17 +34,19 @@ class MainActivity : AppCompatActivity() {
 
         bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemReselectedListener)
     }
-    fun createHomeFragment() {
+    private fun createHomeFragment() {
         val transaction = manager.beginTransaction()
         val frag = HomeFragment()
         transaction.replace(R.id.fragment_container, frag)
         transaction.addToBackStack(null)
+        transaction.commit()
     }
 
-    fun createNotificationFragment() {
+    private fun createNotificationFragment() {
         val transaction = manager.beginTransaction()
         val frag = NotificationFragment()
         transaction.replace(R.id.fragment_container, frag)
         transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
--- app/src/main/java/com/example/emmy_ibook/fragments/NotificationFragment.kt
+++ app/src/main/java/com/example/emmy_ibook/fragments/NotificationFragment.kt
@@ -23,7 +23,7 @@ class NotificationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_notification, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
