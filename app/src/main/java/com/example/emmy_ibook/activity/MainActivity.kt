a6af553fe194e9f4048b001836fabb231fdd2dc2
The_SHOGBAMU <shogbamuolamide@gmail.com>
2021-10-19 20:20:36 +0100

    conflict resolving

--- app/src/main/java/com/example/emmy_ibook/activity/MainActivity.kt
+++ app/src/main/java/com/example/emmy_ibook/activity/MainActivity.kt
@@ -4,38 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.emmy_ibook.R
 import com.example.emmy_ibook.fragments.HomeFragment
+import kotlinx.android.synthetic.main.activity_home_screen.*
 import com.example.emmy_ibook.fragments.NotificationFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_home_screen.*
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
-    val manager = supportFragmentManager
 
-    private val mOnNavigationItemReselectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId){
+    private val mOnNavigationItemReselectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
 
-            R.id.ic_notification -> {
-                createNotificationFragment()
-                return@OnNavigationItemSelectedListener true
+                R.id.ic_notification -> {
+                    createNotificationFragment()
+                    return@OnNavigationItemSelectedListener true
+                }
+
             }
-
+            false
         }
-        false
-    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
 
+        //create default fragment
         createHomeFragment()
 
         bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemReselectedListener)
     }
+
     private fun createHomeFragment() {
-        val transaction = manager.beginTransaction()
+        val transaction = supportFragmentManager.beginTransaction()
         val frag = HomeFragment()
         transaction.replace(R.id.fragment_container, frag)
         transaction.addToBackStack(null)
@@ -43,7 +45,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createNotificationFragment() {
-        val transaction = manager.beginTransaction()
+        val transaction = supportFragmentManager.beginTransaction()
         val frag = NotificationFragment()
         transaction.replace(R.id.fragment_container, frag)
         transaction.addToBackStack(null)
